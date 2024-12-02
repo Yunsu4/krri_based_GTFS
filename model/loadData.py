4dd235e26a3eee1d80361d89747a9620cbc1f4c4
Yunsu4 <ysa020428@gmail.com>
2024-12-02 09:46:22 +0900

    refactor: 간단하게 진행되는 버전
    주석 처리한 부분은 현시각 이후 모든 시각에서 데이터를 찾는 버전

--- model/loadData.py
+++ model/loadData.py
@@ -13,6 +13,7 @@ def load_routes():
 
 
 ''' 현 시각 이후 stop_times 파일 로드(현시각이 23시 인 경우 00h까지만 로드)'''
+'''
 def load_stop_times(user_time):
     import os
     
@@ -73,9 +74,48 @@ def load_stop_times(user_time):
     
     return pd.concat(dfs, ignore_index=True)
 
+'''
+def load_stop_times(user_time):
+    target_time = filter_target_time(user_time)
+
+    if isinstance(target_time, str):
+        stop_times_path = f"resources/stop_times/stop_times_{target_time}h.csv"
+        return pd.read_csv(stop_times_path)
+    else:
+        first_hour_path = f"resources/stop_times/stop_times_{target_time[0]}h.csv"
+        second_hour_path = f"resources/stop_times/stop_times_{target_time[1]}h.csv"
+
+        first_hour_data = pd.read_csv(first_hour_path)
+        second_hour_data = pd.read_csv(second_hour_path)
+
+        return pd.concat([first_hour_data, second_hour_data], ignore_index=True)
+
+
+def filter_target_time(user_time):
+    if((user_time >= '24:00:00') | 
+    ((user_time >= '00:00:00') & (user_time < '05:00:00'))):
+        return "00"
+    # 시간 문자열을 시, 분, 초로 분리
+    hour, minute, second = user_time.split(":")
+
+    # 분과 초가 00인지 확인
+    if minute == "00" and second == "00":
+        return f"{hour}"
+    else:
+        # 다음 시간 계산
+        if hour == "23":
+            next_hour = "00"
+        else: next_hour = str(int(hour) + 1).zfill(2)
+        return [f"{hour}", f"{next_hour}"]
+
+
+
+
+
 
 """데이터 파일들을 로드하는 함수"""
 def load_data():
     stops = pd.read_csv(stops_file_path)
     routes = load_routes()
     return stops, routes
+
